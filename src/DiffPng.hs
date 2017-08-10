55042815b3cb4149437fc14b8962ff257dbf841f
Nicholas Yip <yiplunkwun@gmail.com>
2017-08-10 12:36:09 +0900

    List files by extension

--- src/DiffPng.hs
+++ src/DiffPng.hs
@@ -1,6 +1,18 @@
 module DiffPng where
 
+import Conduit
+import Safe
+import System.FilePath (takeExtension)
+import System.Directory (doesFileExist)
+
+filesByExtension :: MonadResource m => String -> FilePath -> Producer m FilePath
+filesByExtension extension directory = sourceDirectory directory
+  .| filterC ((== Just extension) . tailMay . takeExtension)
+  .| filterMC (liftIO . doesFileExist)
+
 diffPng :: FilePath -> FilePath -> IO ()
 diffPng source target = do
-  putStrLn source
-  putStrLn target
+  runConduitRes $ entries source
+  runConduitRes $ entries target
+  where
+    entries directory = filesByExtension "png" directory .| mapM_C (liftIO . putStrLn)
