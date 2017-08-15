10fc7d4f998a3c70e554a8550b5aec2115c8fafd
Nicholas Yip <yiplunkwun@gmail.com>
2017-08-16 05:37:48 +0900

    Implement diffSet

--- src/DiffPng.hs
+++ src/DiffPng.hs
@@ -7,6 +7,13 @@ import Safe (tailMay)
 import System.Directory (doesFileExist)
 import System.FilePath (takeExtension)
 
+data DiffSet = DiffSet
+  { sourceRemainder :: [FilePath]
+  , targetRemainder :: [FilePath]
+  , diffEntries :: [(FilePath, FilePath)]
+  }
+  deriving Show
+
 filesByExtension :: MonadResource m => String -> FilePath -> Producer m FilePath
 filesByExtension extension directory = sourceDirectory directory
   .| filterC ((== Just extension) . tailMay . takeExtension)
@@ -15,8 +22,12 @@ filesByExtension extension directory = sourceDirectory directory
 candidates :: FilePath -> IO [FilePath]
 candidates directory = sortOn sortKey <$> runConduitRes (filesByExtension "png" directory .| sinkList)
 
-printEntries :: FilePath -> IO ()
-printEntries directory = print =<< candidates directory
+diffSet :: [FilePath] -> [FilePath] -> DiffSet
+diffSet a b = DiffSet a2 b2 $ a1 `zip` b1
+  where
+    bb `bisect` aa = splitAt (length bb) aa
+    (a1, a2) = b `bisect` a
+    (b1, b2) = a `bisect` b
 
 diffPng :: FilePath -> FilePath -> IO ()
-diffPng source target = printEntries source >> printEntries target
+diffPng source target = print =<< diffSet <$> candidates source <*> candidates target
