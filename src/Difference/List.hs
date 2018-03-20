59de9fe860dd6bcac400729e316c1f4a815dbdcc
Nicholas Yip <yiplunkwun@gmail.com>
2018-03-21 06:37:37 +0900

    Add sorting logics to the list comparison

--- src/Difference/List.hs
+++ src/Difference/List.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
 module Difference.List where
 
+import Data.List (sortOn)
 import Difference (DifferenceT (..))
 
 data SaturatedZip a = SaturatedZip {
@@ -9,11 +10,13 @@ data SaturatedZip a = SaturatedZip {
 }
 
 
-instance DifferenceT [] SaturatedZip a where
-  differenceT _ p q
-    | np > nq = unbalanced Left (`zip` q) $ splitAt nq p
-    | otherwise = unbalanced Right (p `zip`) $ splitAt np q
+instance Ord b => DifferenceT (a -> b) [] SaturatedZip a where
+  differenceT sorting p q
+    | np > nq = unbalanced Left (`zip` q') $ splitAt nq p'
+    | otherwise = unbalanced Right (p' `zip`) $ splitAt np q'
     where
+      p' = sortOn sorting p
+      q' = sortOn sorting q
       np = length p
       nq = length q
       unbalanced side zipping (a1, a2) = SaturatedZip (side a2) (zipping a1)
