f6fa57d5fa404e5e790a19a13dbbafc4a1ece34d
Nicholas Yip <yiplunkwun@gmail.com>
2018-03-22 07:49:11 +0900

    Implement writing image difference

--- src/Difference/Image/File.hs
+++ src/Difference/Image/File.hs
@@ -2,15 +2,17 @@
 module Difference.Image.File where
 
 import Codec.Picture.Types (Image)
+import Control.Applicative (liftA2)
 import Data.Functor.Identity (Identity)
+import Data.Function (on)
 import System.FilePath ((</>), takeBaseName)
 
 import Difference (Difference (..), DifferenceT (..))
 import Difference.Directory.Extension (FileExtension (..))
 import Difference.Image.Color (ColorComparison)
 
-data FileNameOptions =
-  FileNameOptions {
+data FileOptions =
+  FileOptions {
     directory :: FilePath,
     prefix :: String,
     extension :: FileExtension
@@ -20,13 +22,17 @@ data ImageOptions a =
   ImageOptions {
     readImage :: FilePath -> IO (Image a),
     writeImage :: FilePath -> Image a -> IO (),
-    colorComparison :: ColorComparison
+    colorComparison :: ColorComparison,
+    fileOptions :: FileOptions
   }
 
 
-instance Difference (FileNameOptions, Int) FilePath where
-  difference (FileNameOptions directory prefix extension, i) p q =
+instance Difference (FileOptions, Int) FilePath where
+  difference (FileOptions directory prefix extension, i) p q =
     directory </> prefix ++ show i ++ ' ' : takeBaseName p ++ ' ' : takeBaseName q ++ '.' : name extension
 
-instance Difference ColorComparison (Image a) => DifferenceT (ImageOptions a) IO FilePath where
-  differenceT = undefined -- TODO
+instance Difference ColorComparison (Image a) => DifferenceT (ImageOptions a, Int) IO FilePath where
+  differenceT (ImageOptions read write c f, i) p q = write' (difference (f, i) p q) =<< p `diff` q
+    where
+      diff = liftA2 (difference c) `on` read
+      write' file = (file <$) . write file
