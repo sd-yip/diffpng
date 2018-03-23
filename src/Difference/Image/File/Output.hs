684951446f8e7ff3b363a09ef619facb40e7e2e2
Nicholas Yip <yiplunkwun@gmail.com>
2018-03-23 20:03:00 +0900

    'writeGenerated'

--- src/Difference/Image/File/Output.hs
+++ src/Difference/Image/File/Output.hs
@@ -1,4 +1,9 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Difference.Image.File.Output where
+
+import Difference (Difference (..))
+import Difference.File.BasePath (BaseFilePath)
+import Difference.Image.File (ImageOptions)
 
 {-
 writePlainCopy :: FilePath -> Int -> String -> FilePath -> IO ()
@@ -17,3 +22,9 @@ writeLeftovers (FileDiff ra rb e) = writeCopies "a" ra *> writeCopies "b" rb
   where
     writeCopies code = consumeIndexed (length e) $ \(i, path) -> writePlainCopy "leftovers" i code path
 -}
+
+writeGenerated :: Difference (ImageOptions a, Int) FilePath (IO FilePath) =>
+    Int -> ImageOptions a -> [(FilePath, FilePath)] -> IO [FilePath]
+writeGenerated startIndex options inputs = consume `traverse` (inputs `zip` [startIndex..])
+  where
+    consume ((p, q), i) = difference (options, i) p q
