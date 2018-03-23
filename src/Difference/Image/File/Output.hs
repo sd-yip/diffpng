4f17cc1943f71cd5fe1262011c852d1d8854a6ad
Nicholas Yip <yiplunkwun@gmail.com>
2018-03-23 22:17:09 +0900

    Add space in the range notations

--- src/Difference/Image/File/Output.hs
+++ src/Difference/Image/File/Output.hs
@@ -12,13 +12,13 @@ import Difference.Image.File (ImageOptions)
 
 writeGenerated :: Difference (ImageOptions a, Int) FilePath (IO FilePath) =>
     Int -> ImageOptions a -> [(FilePath, FilePath)] -> IO [FilePath]
-writeGenerated startIndex options inputs = consume `P.mapM` (inputs `zip` [startIndex..])
+writeGenerated startIndex options inputs = consume `P.mapM` (inputs `zip` [startIndex ..])
   where
     consume ((p, q), i) = difference (options, i) p q
 
 writeOriginal :: Int -> (FileOptions, String) -> [FilePath] -> IO [FilePath]
 writeOriginal startIndex (FileOptions directory prefix extension, code) inputs =
-  consume `traverse` (inputs `zip` [startIndex..])
+  consume `traverse` (inputs `zip` [startIndex ..])
     where
       copyFile' source target = target <$ copyFile source target <* createParentDirectories target
       consume (originalFile, i) = copyFile' originalFile $ unBaseFilePath directory
