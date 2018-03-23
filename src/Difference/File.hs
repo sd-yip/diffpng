e1160e88727e9ae245baf7a166cd1a5d430e0f55
Nicholas Yip <yiplunkwun@gmail.com>
2018-03-23 19:45:54 +0900

    Implement generateOutputs

--- src/Difference/File.hs
+++ src/Difference/File.hs
@@ -3,16 +3,17 @@ module Difference.File where
 import System.FilePath ((</>), takeBaseName)
 
 import Difference (DifferenceI (..))
+import Difference.File.BasePath (BaseFilePath (..))
 import Difference.File.Extension (FileExtension)
 
 data FileOptions =
   FileOptions {
-    directory :: FilePath,
+    directory :: BaseFilePath,
     prefix :: String,
     extension :: FileExtension
   }
 
 
 instance DifferenceI (FileOptions, Int) FilePath where
-  differenceI (FileOptions directory prefix extension, i) p q =
-    directory </> prefix ++ show i ++ ' ' : takeBaseName p ++ ' ' : takeBaseName q ++ '.' : show extension
+  differenceI (FileOptions directory prefix extension, i) p q = unBaseFilePath directory
+      </> prefix ++ show i ++ ' ' : takeBaseName p ++ ' ' : takeBaseName q ++ '.' : show extension
--- src/Difference/Image/File/Generation.hs
+++ src/Difference/Image/File/Generation.hs
@@ -1,29 +1,17 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Difference.Image.File.Generation where
 
 import Difference (Difference (..))
 import Difference.File.BasePath (BaseFilePath)
-import Difference.Image.Color (ColorComparison)
+import Difference.Image.File (ImageOptions)
 
 {-
-consumeIndexed :: Int -> ((Int, a) -> IO ()) -> [a] -> IO ()
-consumeIndexed initialIndex consumer = (consumer `mapM_`) . zip [initialIndex..]
-
 createParentDirectories :: FilePath -> IO ()
 createParentDirectories = takeDirectory >>> createDirectoryIfMissing True -- Recursively
-
-writeDiffs :: (String, PixelRGBA8 -> PixelRGBA8 -> PixelRGBA8) -> [(FilePath, FilePath)] -> IO ()
-writeDiffs (prefix, mixing) = consumeIndexed 0 $ \(i, (a, b)) -> do
-  pixels <- diff mixing <$> readRGBA a <*> readRGBA b
-  let targetPath = prefix </> unpack [lt|#{over _head toUpper prefix}#{show i} #{takeBaseName a} #{takeBaseName b}.png|]
-  createParentDirectories targetPath *> writePng targetPath pixels
 -}
 
-data GenerationOptions =
-  GenerationOptions {
-    colorComparison :: ColorComparison,
-    outputPath :: BaseFilePath
-  }
-
-
-instance Difference GenerationOptions [FilePath] (IO [FilePath]) where
-  difference _ _ _ = undefined
+generateOutputs :: Difference (ImageOptions a, Int) FilePath (IO FilePath) =>
+    Int -> ImageOptions a -> [(FilePath, FilePath)] -> IO [FilePath]
+generateOutputs startIndex options inputs = consume `traverse` (inputs `zip` [startIndex..])
+  where
+    consume ((p, q), i) = difference (options, i) p q
